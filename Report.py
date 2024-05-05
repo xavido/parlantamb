b44e182e37fb09bf4c62f749585b1eca0915b898
Xavi Domínguez <xavi@fablabbcn.org>
2024-05-05 22:05:05 +0200

    Update Report.py

--- Report.py
+++ Report.py
@@ -187,7 +187,7 @@ if st.session_state.start_chat:
             if df['infografia'][i]:
                 df['infografia'][i] = "https://www.xavidominguez.com/tecla/"+str(df['infografia'][i])
                 #st.write(df['infografia'][i])
-                listimages.append("'https://www.xavidominguez.com/tecla/"+str(df['infografia'][i])+"'")
+                listimages.append(str(df['infografia'][i]))
                 listcaptions.append('Test')
 
         st.image(listimages,caption=listcaptions,width=200,output_format="JPEG")
