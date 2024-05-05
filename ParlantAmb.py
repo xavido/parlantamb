b33f1e8b2bee14e1c3fad0089b800fc2c7976410
Xavi Domínguez <xavi@fablabbcn.org>
2024-05-05 15:24:48 +0200

    Update ParlantAmb.py

--- ParlantAmb.py
+++ ParlantAmb.py
@@ -70,7 +70,7 @@ def disable():
         if nom != '':
             st.sidebar.write(":red[Este usuario no existe]")
         if nom in l2:
-            especials = "Find the answer in the document.Give half of the answer in lower case and the other half in capitals."
+            especials = "Answer always in spanish."
         if nom in l3:
             especials3 = "Gives answers only about art in Ancient Egypt.Find the answer in the document in the section: 'EL ARTE EN EL ANTIGUO EGIPTO: pintura y escultura.'"
         if nom in l4:
