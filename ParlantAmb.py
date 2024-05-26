02d0b309fd6597d59747c81745caaa22793cf99e
Xavi Domínguez <xavi@fablabbcn.org>
2024-05-26 10:42:22 +0200

    Update ParlantAmb.py

--- ParlantAmb.py
+++ ParlantAmb.py
@@ -140,12 +140,13 @@ if st.session_state.start_chat:
         with st.chat_message(message["role"]):
             st.markdown(message["content"])
 
+    stt_user =''
     stt_user = speech_to_text(language='en', use_container_width=True, just_once=True, key='STT')
 
     if prompt := st.chat_input("Escribe aquí tu pregunta") or stt_user != '':
         if stt_user !='':
             prompt = stt_user
-            
+
         st.session_state.messages.append({"role": "user", "content": prompt})
         with st.chat_message("user"):
             st.markdown(prompt)
